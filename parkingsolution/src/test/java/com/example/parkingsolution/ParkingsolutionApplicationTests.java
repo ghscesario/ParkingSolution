a59e5b3fb1b07d75409054a4ece8078f52feec1b
ghscesario <gabriel.hscesario>
2025-02-19 21:09:00 -0300

    Inclusão de colunas e linhas

---
+++ parkingsolution/src/test/java/com/example/parkingsolution/ParkingsolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parkingsolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingsolutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
